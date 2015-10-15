358faf3f5c449f6efacb261a78c4067e947fc9b7
lovelonger <tanmingxiao@gmail.com>
2015-10-15 10:26:00 +0800

    Update install.sh
    添加nginx默认首页

--- modules/nginx/install.sh
+++ modules/nginx/install.sh
@@ -3,6 +3,8 @@
 yum -y --enablerepo=nginx install nginx
 rm -rf /etc/nginx/*
 cp -a conf/* /etc/nginx
+mkdir -p /opt/www
+cp -r /usr/share/nginx/html/* /opt/www
 
 cat > /etc/supervisord.d/nginx.conf << EOF
 [program:nginx]
