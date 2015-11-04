e298119c84fbcab92b5a13d983cacb4412284d2e
lovelonger <tanmingxiao@gmail.com>
2015-11-04 16:01:48 +0800

    修改jdk下载链接

--- modules/jdk7/install.sh
+++ modules/jdk7/install.sh
@@ -4,7 +4,7 @@ pkg='jdk-7u80-linux-x64.tar.gz'
 ver='jdk1.7.0_80'
 dest='/opt/javahome'
 
-[ -f "../cache/${pkg}" ] && pkg="../cache/${pkg}" || wget https://www.reucon.com/cdn/java/${pkg}
+[ -f "../cache/${pkg}" ] && pkg="../cache/${pkg}" || wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/7u80-b15/${pkg}"
 
 cp jdk.sh /etc/profile.d/
 mkdir -p ${dest}
